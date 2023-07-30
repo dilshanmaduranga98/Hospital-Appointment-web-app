59d78459c1385b75a5100b10d22edb2b9d973e94
dilshanmaduranga98 <gamagedilshan98@gmail.com>
2023-07-30 14:29:51 +0530

    hnage-title

--- HTML/form.php
+++ HTML/form.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/Style.css?v=<?php echo time(); ?>">
-    <title>AdvertiseMe</title>
+    <title>WintanCare</title>
 </head>
 <body class="main_class_form">
     <?php include 'initial.php'; ?>
